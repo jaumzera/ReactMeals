6d0b6e594dec5c22e0034be5bfb97009a25d82b5
Jaumzera <joaomassan@gmail.com>
2023-03-29 10:48:35 -0300

    First commit meals app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/layout/Header';
+import Meals from './components/meals/Meals';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
